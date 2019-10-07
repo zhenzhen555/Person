e804dd78e733d9b458c7770c1e737aa1e8329d0e
zhenzhen555 <3192155218@qq.com>
2019-10-07 11:24:43 +0800

    新增PackageTest

---
+++ PackageTest/src/PackageTest.java
@@ -0,0 +1,12 @@
+import course.*;
+import teacher.*;
+import student.*;
+
+
+public class PackageTest{
+	public static void main(String[] args){
+		Course cs = new Course();
+		Teacher te = new Teacher();
+		Student st = new Student();
+	}
+}
