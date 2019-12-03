da8fedcaef3c63e93e3104d9774184993b0bac9b
zhenzhen555 <3192155218@qq.com>
2019-12-03 19:41:02 +0800

    代码重构2

---
+++ 代码重构2/re/Rental.java
@@ -0,0 +1,29 @@
+package re;
+import mv.*;
+public class Rental{
+    private Movie _movie;
+    private int _daysRented;
+   // private double getCharge;
+  public double getCharge() {
+    	      return _movie.getCharge(_daysRented);
+    	
+     }
+    public Rental(Movie movie, int daysRented){
+     _movie = movie;
+     _daysRented = daysRented;
+    }
+     public int getDaysRented() {
+       return _daysRented;
+     }
+    public Movie getMovie(){
+       return _movie;
+     }
+    public int  getFrequentRenterPoints() {
+    	 if((getMovie().getPriceCode()==Movie.NEW_RELEASE)&&getDaysRented()>1)
+    		 return 2;
+    	 else 
+    		 return 1;
+    }
+	
+   
+}
