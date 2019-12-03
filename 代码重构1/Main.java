ee4d88e728cf82f1c5a96c6aadefd8a0d9a27bd1
zhenzhen555 <3192155218@qq.com>
2019-12-03 18:10:40 +0800

    代码重构

---
+++ 代码重构1/Main.java
@@ -0,0 +1,15 @@
+import cu.*;
+import mv.*;
+import re.*;
+
+public class Main{
+	public static void main(String[] args) {
+		/*Movie m1=new Movie("Jack",2);
+		Movie m1=new Movie("Jack",2);
+		Movie m1=new Movie("Jack",2);
+		*/
+		Customer c1=new Customer("LiMING");
+		c1.addRental(new Rental(new Movie("jack",2),6));
+		System.out.print(c1.statement());
+	}
+}
